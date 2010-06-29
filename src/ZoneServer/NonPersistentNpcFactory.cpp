edfd846fa59f8fe7b192178685b36770bad8a09f
kbrooker99206 <kbrooker@samasdesigns.net>
2010-06-29 11:45:23 -0700

    enum fix

--- src/ZoneServer/NonPersistentNpcFactory.cpp
+++ src/ZoneServer/NonPersistentNpcFactory.cpp
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "NonPersistentNpcFactory.h"
+#include "PlayerEnums.h"
 #include "AttackableCreature.h"
 #include "AttackableStaticNpc.h"
 #include "FillerNPC.h"
